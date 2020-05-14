053e09baa0de7c80d1a3ff210d43e3574009c2f4
susilo hartomo <encusilo545110@gmail.com>
2020-05-14 13:44:01 +0700

    awal pair project brance  dev

---
+++ client/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <BlockGame/>
+    <ButtonMove/>
+  </div>
+</template>
+
+<script>
+import BlockGame from "./components/BlockGame.vue";
+
+export default {
+  name: "App",
+  components: {
+    BlockGame
+  },
+ 
+};
+</script>
+
+<style>
+</style>
